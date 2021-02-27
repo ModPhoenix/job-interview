0491a071a2c124de376b56e38c1207b7a38089f9
ModPhoenix <ivanchenko.b@gmail.com>
2021-02-27 23:35:34 +0200

    Handle error for decode_token

--- src/auth/managers.rs
+++ src/auth/managers.rs
@@ -1,5 +1,6 @@
 use super::models::{Claims, Role};
 use actix_web::HttpRequest;
+use async_graphql::Error;
 use chrono::{Duration, Local};
 use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, TokenData, Validation};
 use lazy_static::lazy_static;
@@ -30,24 +31,33 @@ pub fn create_token(username: String, role: Role) -> String {
 }
 
 pub fn get_role(http_request: HttpRequest) -> Option<Role> {
-    http_request
+    let token = http_request
         .headers()
         .get("Authorization")
         .and_then(|header_value| {
             header_value.to_str().ok().map(|s| {
                 let jwt_start_index = "Bearer ".len();
                 let jwt = s[jwt_start_index..s.len()].to_string();
-                let token_data = decode_token(&jwt);
-                Role::from_str(&token_data.claims.role).expect("Can't parse role")
+                return jwt;
             })
-        })
+        });
+
+    if let Some(jwt) = token {
+        let token_data = decode_token(&jwt).ok()?;
+        println!("{:?}", token_data);
+        let role = Role::from_str(&token_data.claims.role).expect("Can't parse role");
+        println!("{:?}", role);
+        Some(role)
+    } else {
+        None
+    }
 }
 
-fn decode_token(token: &str) -> TokenData<Claims> {
+fn decode_token(token: &str) -> Result<TokenData<Claims>, Error> {
     decode::<Claims>(
         &token,
         &DecodingKey::from_secret(JWT_SECRET_KEY.as_ref()),
         &Validation::default(),
     )
-    .expect("Can't decode token")
+    .map_err(|_e| Error::new("Can't decode token"))
 }
