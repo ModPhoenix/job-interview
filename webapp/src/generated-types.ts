b43a44aefbc8c0c4a6cf8a6bf97d69214c7243d2
ModPhoenix <ivanchenko.b@gmail.com>
2021-03-10 21:48:34 +0200

    Add useSignInMutation

--- webapp/src/generated-types.ts
+++ webapp/src/generated-types.ts
@@ -114,6 +114,16 @@ export type User = {
   createdAt: Scalars['NaiveDateTime'];
 };
 
+export type SignInMutationVariables = Exact<{
+  input: SignInInput;
+}>;
+
+
+export type SignInMutation = (
+  { __typename?: 'MutationRoot' }
+  & Pick<MutationRoot, 'signIn'>
+);
+
 export type QuestionsQueryVariables = Exact<{ [key: string]: never; }>;
 
 
@@ -161,6 +171,36 @@ export const NewQuestionFragmentDoc = gql`
   body
 }
     `;
+export const SignInDocument = gql`
+    mutation SignIn($input: SignInInput!) {
+  signIn(input: $input)
+}
+    `;
+export type SignInMutationFn = Apollo.MutationFunction<SignInMutation, SignInMutationVariables>;
+
+/**
+ * __useSignInMutation__
+ *
+ * To run a mutation, you first call `useSignInMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useSignInMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [signInMutation, { data, loading, error }] = useSignInMutation({
+ *   variables: {
+ *      input: // value for 'input'
+ *   },
+ * });
+ */
+export function useSignInMutation(baseOptions?: Apollo.MutationHookOptions<SignInMutation, SignInMutationVariables>) {
+        return Apollo.useMutation<SignInMutation, SignInMutationVariables>(SignInDocument, baseOptions);
+      }
+export type SignInMutationHookResult = ReturnType<typeof useSignInMutation>;
+export type SignInMutationResult = Apollo.MutationResult<SignInMutation>;
+export type SignInMutationOptions = Apollo.BaseMutationOptions<SignInMutation, SignInMutationVariables>;
 export const QuestionsDocument = gql`
     query Questions {
   questions {
